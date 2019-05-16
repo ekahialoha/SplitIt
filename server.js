c0dbd4a32bfdc85704ae18a19c20a3a0285c2ef1
Matt Talamantes <matthew@Matts-MacBook-Pro.local>
2019-05-16 16:55:43 -0700

    setup server.js and connected to mongo

---
+++ server.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const sessions = require('express-session');
+const PORT = process.env.PORT;
+const MONGODB = process.env.MONGODB;
+
+
+app.use(express.static('public'));
+app.use(express.json());
+app.use(sessions({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.listen(PORT, () => {
+    console.log("I'm listening...");
+})
+
+mongoose.connect(MONGODB, {useNewUrlParser:true});
+mongoose.connection.once('open', ()=>{
+    console.log('connected to mongo');
+})
