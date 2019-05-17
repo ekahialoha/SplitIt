0b3acfcf3f45a3a425f6d9225ab599fed16e69f1
ecshaff <emmyshaff@gmail.com>
2019-05-17 13:58:49 -0700

    updated house js model

--- models/house.js
+++ models/house.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const houseSchema = new mongoose.Schema({
 	name: {type: String, required: true},
 	owner: {type: Schema.Types.ObjectId, ref: 'User', default:'' },
-	members: [{type: Schema.Types.ObjectId, ref: 'User', default:[] }];
+	members: [{type: Schema.Types.ObjectId, ref: 'User', default:[] }]
 
 });
 
